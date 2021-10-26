01f4081a404bf3bf9bc5d2896a190402ac62bc9e
Esteph27 <esteph.csa@gmail.com>
2021-10-26 11:43:28 +0000

    add update_client function to print client info to terminal

--- run.py
+++ run.py
@@ -1,46 +1,61 @@
-# dictionaries;
+from pprint import pprint
 
-# salon_dates shows the avaible dates and times of the salon
+""" salon_dates shows the days and time avaible for the salon. These will be used to check against the user input days and time to see if there id availabilty for the requested time and day."""
+
 salon_dates = {
-  "Monday" : {
-    "Morning" : ["10:00", "11:00", "12:00"],
-    "Afternoon" : ["13:00", "14:00", "15:00", "16:00", "17:00"],
+  "Monday": {
+    "Morning": ["10:00", "11:00", "12:00"],
+    "Afternoon": ["13:00", "14:00", "15:00"],
   },
-  "Tuesday" : {
-    "Morning" : ["10:00", "11:00", "12:00"],
-    "Afternoon" : ["13:00", "14:00", "15:00", "16:00", "17:00"],
+  "Tuesday": {
+    "Morning": ["10:00", "11:00", "12:00"],
+    "Afternoon": ["13:00", "14:00", "15:00"],
   },
-  "Wednesday" : {
-    "Morning" : ["10:00", "11:00", "12:00"],
-    "Afternoon" : ["13:00", "14:00", "15:00", "16:00", "17:00"],
+  "Wednesday": {
+    "Morning": ["10:00", "11:00", "12:00"],
+    "Afternoon": ["13:00", "14:00", "15:00"],
   },
-  "Thursday" : {
-    "Morning" : ["10:00", "11:00", "12:00"],
-    "Afternoon" : ["13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"],
+  "Thursday": {
+    "Morning": ["10:00", "11:00", "12:00"],
+    "Afternoon": ["13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20;00", "21:00"],
   },
-  "Friday" : {
-    "Morning" : ["10:00", "11:00", "12:00"],
-    "Afternoon" : ["13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00"]
+  "Friday": {
+    "Morning": ["10:00", "11:00", "12:00"],
+    "Afternoon": ["13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00"]
   }
 }
 
-# services shows the services provided by the salon with the price for each service
+""" services shows the services provided by the salon with the price for each service. This will be used to calculate how much the client's appointment will be.
+For scalability, the user would be given the option to update this dictionary with more treatments/servcies which the salon decides to have in future as they also upscale their business"""
+
 services = {
-  "Cut and Blow Dry" : 50,
-  "Half head Highlights" : 50,
-  "Highlights" : 100,
-  "Tint" : 20,
-  "Hair cut" : 40,
-  "Colour" : 60,
-  "Treatment conditioner" : 20 
+  "Cut and Blow Dry": 50,
+  "Half head Highlights": 50,
+  "Highlights": 100,
+  "Tint": 20,
+  "Hair cut": 40,
+  "Colour": 60,
+  "Treatment conditioner": 20
 }
 
-# client shows client information
-clients = {
-    "first name" : [],
-    "last name" : [],
-    "email" : [],
-    "servies required" : []
+""" client shows takes in the client informaiton provided by the user.
+for scalability, this informaiton will be connected to an API and logged to database for the business to keep track on client histroy."""
+client_info = {
+    "first name": [],
+    "last name": [],
+    "email": [],
+    "contact number": []
+}
+
+""" client_appointment shows the appointment details for the client in question. the terminal will print the values once the apppoinmnet has been made.
+# for scalability, this informaiton would be uploaded onto a database and the client will also recive an email/text notification with this information"""
+
+clients_appointments = {
+  "client name": [],
+  "services requested": [],
+  "appointment date": [],
+  "booking number": []
+
 }
 
 #-------------------------------------------------------------------->
@@ -55,55 +70,58 @@ print(dash.center(50))
 #-------------------------------------------------------------------->
 # request appointment and client information from the user;
 
-print("To book a new appointment, please provide the below information.\n")
+print("To book a new appointment, please provide the below information;\n")
 
-print("Please add in client's first name")
+print("Please provide client's first name.")
 fname = input("First Name:\n")
 
-print("Please add in client's last name")
+print("Please provide client's last name.")
 lname = input("Last Name:\n")
 
-print("Please add in client's email address")
+print("Please provide client's email address.")
 user_email = input("Email:\n")
 
-print("Please add in client's contact number")
+print("Please provide client's contact number")
 contact_no = input("Contact Number:\n")
 
-print("Please provide client's available day, type name of day,for example, Tuesday")
-user_day = input("Day:\n")
-
-print("Please provide client's available time, tye time in digital 24hr format, for example 14:00")
-user_time = str(input("Time:\n"))
-
-print("Please provide the required service seperated by commas, for example; highlights, cut and blow dry\n")
-services = input("Service(s):\n")
-
-print("Thank you. Getting information....\n")
-
-
-def available_dates():
+def update_client():
     """
-    works out if given user time and date are available. if day and time are available, the user will be notified. If there is no availability for the day and time provided, the program will calculate next avaible day and time
+    adds client informaiton to clients dictionary and prints to terminal
     """
-    print("Checking appointment availability....\n")
 
-    pass
+    client_info.update({"first name":f"{fname}", "last name":f"{lname}", "email":f"{user_email}", "contact number":f"{contact_no}"})
 
-    # for values in salon_dates:
-    #     print(values)
+    for info in client_info.values():
+      print(info)
 
-    
+    print("client added!")
 
-    #print(salon_dates["Monday"]["Morning"][0] == user_day)
+update_client()
 
-available_dates()
+# print("Continue to booking./n")
+
+# print("Please provide client's available day, type name of day,for example, Tuesday")
+# user_day = input("Day:\n")
+
+# print("Please provide client's available time, tye time in digital 24hr format, for example 14:00")
+# user_time = str(input("Time:\n"))
+
+# print("Please provide the required service seperated by commas, for example; highlights, cut and blow dry\n")
+# services = input("Service(s):\n")
+
+# print("Thank you. Getting information....\n")
+
+
+# def available_dates():
+#     """
+#     works out if given user time and date are available. if day and time are available, the user will be notified. If there is no availability for the day and time provided, the program will calculate next avaible day and time
+#     """
+#     print("Checking appointment availability....\n")
+#     pass
+
+
+
+# available_dates()
 
 print("Appointment available!")
 print("Calculating price and generating unqiue booking numnber...")
-
-
-#-------------------------------------------------------------------->
-# functions
-
-def unique_booking_ref():
-    pass
