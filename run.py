b74f6667b27c073b706773d150726ebb6b94cabf
Esteph27 <esteph.csa@gmail.com>
2021-10-28 16:55:24 +0000

    refacture structure

--- run.py
+++ run.py
@@ -7,7 +7,14 @@ import string
 # this will be used to check the salon availability against the user input
 
 salon_dates = {
-  "monday": ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00"],
+    "monday": [
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00", 
+        "14:00", 
+        "15:00"
+    ],
 
   "tuesday": ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00"],
 
@@ -47,149 +54,140 @@ client_app = {
   "services requested": [],
   "appointment date": [],
   "booking number": [],
-  "total cost" : []
+  "total cost": []
 }
 
-# -------------------------------------------------------------------->
-# start of program;
-
-INTRO = "Welcome to the Salon Booking App!\n"
-print(INTRO.center(50))
-
 DASH = "-" * 35
-print(DASH.center(50))
-
 SPACE = " "
 
-# -------------------------------------------------------------------->
-# collect customer information;
 
-print("To book a new appointment, please provide the below information;\n")
-print("Please provide client's first name.\n")
-fname = input("First Name:\n")
-print(SPACE)
+def customer_info():
+    # -------------------------------------------------------------------->
+    # collect customer information;
 
-print("Please provide client's last name.\n")
-lname = input("Last Name:\n")
-print(SPACE)
+    print("To book a new appointment, please provide the below information;\n")
+    print("Please provide client's first name.\n")
+    fname = input("First Name:\n")
+    print(SPACE)
 
-print("Please provide client's email address.\n")
-user_email = input("Email:\n")
-print(SPACE)
+    print("Please provide client's last name.\n")
+    lname = input("Last Name:\n")
+    print(SPACE)
 
-print("Please provide client's contact number.\n")
-contact_no = input("Contact Number:\n")
-print(SPACE)
+    print("Please provide client's email address.\n")
+    user_email = input("Email:\n")
+    print(SPACE)
 
-# adds client information to client_info
-client_info.update({"first name": f"{fname}", "last name": f"{lname}", "email": f"{user_email}", "contact number": f"{contact_no}"})
+    print("Please provide client's contact number.\n")
+    contact_no = input("Contact Number:\n")
+    print(SPACE)
 
-# update client_ app with client name
-client_app.update({"client name":f"{fname} " + f"{lname}"})
+    # adds client information to client_info
+    client_info.update({
+        "first name": f"{fname}",
+        "last name": f"{lname}",
+        "email": f"{user_email}",
+        "contact number": f"{contact_no}"
+    })
 
-print(DASH.center(50))
+    # update client_ app with client name
+    client_app.update({"client name": f"{fname} " + f"{lname}"})
 
-for info in client_info.values():
-    print(info)
+    print(DASH.center(50))
 
-print(SPACE)
-print("Client added!")
-print(DASH.center(50))
-print("Continue to appointment details...")
-print(SPACE)
+    for info in client_info.values():
+        print(info)
 
-# -------------------------------------------------------------------->
-# collect appointment information from the user;
+    print(SPACE)
+    print("Client added!")
+    print(DASH.center(50))
+    print("Continue to appointment details...")
+    print(SPACE)
 
 
-def salon_availability(day):
-    """gets available day and provides times available for the day"""
+def appointment_info():
+    """collect appointment information from the user"""
 
-    print("Available time on " + day.upper() + " is: ")
-    print(salon_dates[day.lower()])
+    def _list_of_services():
+        """prints out lsit of services provided by the salon"""
+
+        salon_list = [str(key) for key in service_type]
+        print(salon_list)
+
+    def _calc_cost(selection):
+
+        list_services = selection.split(",")
+
+        total = 0
+        for i in range(len(list_services)):
+            total += service_type[list_services[i]]
+        return total
+
+    user_day = input("Please provide requested day (Monday to Friday):\n")
+
+    print("Available time on " + user_day.upper() + " is: ")
+    print(salon_dates[user_day.lower()])
+
+    user_time = input("Please select a time for " f"{user_day}" ":\n")
     print(user_time)
 
+    print("Thank you. Please now enter the services requested, if there are more than one, please separate them by a comma with no spaces.\n")
+    print(SPACE)
+    print("Here is the list of services:\n")
 
-print("Thank you. Please now enter the services requested, if there are more than one, please separate them by a comma with no spaces.\n")
-print(SPACE)
-print("Here is the list of services:\n")
+    _list_of_services()
 
+    user_selection = input("Enter services requested: \n")
 
-def list_of_services():
-  """prints out lsit of services provided by the salon"""
+    # update client_ app with services input
+    client_app.update({"services requested": f" {user_selection}"})
 
-  salon_list = [str(key) for key in service_type]
-  print(salon_list)
+    total_cost = _calc_cost(user_selection)
+    print("Total will be: " + str(total_cost))
 
-
-def calc_cost(service_names):
-  """works out how much the appointment will cost"""
- 
-  list_services = service_names.split(",")
-
-  print(len(list_services))
-
-  total = 0
-  for i in range(len(list_services)):
-    print ("the total bill for " + list_services[i] + " is: " + str(service_type[list_services[i]]))
-
-    total += service_type[list_services[i]]
-
-    print("Total will be: " + str(total))
+    # update client_ app with total cost;
+    # client_app.update({"total cost": " £" f"{str(total)}"})
 
 
-# update client_ app with total cost;
-# client_app.update({"total cost": " £" f"{str(total)}"})
+def confirm_booking():
+    # -------------------------------------------------------------------->
+    # generate booking id number;
+
+    print("Booking ID number:")
+
+    random_num = random.randint(1, 99)
+    print(random_num)
+
+    random_letter = random.choice(string.ascii_letters) * 2
+    print(random_letter)
+
+    id_num = f"{random_num}" + f"{random_letter}"
+    print(id_num)
+
+    # update clients_ app with booking number
+    client_app.update({"booking number": f" {id_num}"})
+
+    # -------------------------------------------------------------------->
+    # confirm appointment booking;
+
+    print("Booking completed!\nBooking details below:")
+    print(client_app)
+
+    # clients_app.update({})
 
 
-user_day = input("Please provide requested day (Monday to Friday):\n")
+def main():
+    """calls main functions"""
 
-user_time = input("Please select a time for " f"{user_day}" ":\n")
+    intro = "Welcome to the Salon Booking App!\n"
+    print(intro.center(50))
 
-user_selection = input("Enter services requested, : \n")
+    print(DASH.center(50))
 
-# update client_ app with services input
-client_app.update({"services requested":f" {user_selection}"})
-
-
-# -------------------------------------------------------------------->
-# generate booking id number;
-
-print("Booking ID number:")
-
-random_num = random.randint(1, 99)
-print(random_num)
-
-random_letter = random.choice(string.ascii_letters) * 2
-print(random_letter)
+    customer_info()
+    appointment_info()
+    confirm_booking()
 
 
-id_num = f"{random_num}" + f"{random_letter}"
-print(id_num)
-
-# update clients_ app with booking number
-client_app.update({"booking number":f" {id_num}"})
-
-# -------------------------------------------------------------------->
-# confirm appointment booking;
-
-print("Booking completed!\nBooking details below:")
-print(client_app)
-
-# clients_app.update({})
-
-# # clients_app = {
-# #   "client name": [],
-# #   "services requested": [],
-# #   "appointment date": [],
-# #   "booking number": [],
-# #   "booking cost" : []
-# # }
-
-# -------------------------------------------------------------------->
-# funcitons:
-
-salon_availability(user_day)
-list_of_services()
-calc_cost(user_selection)
-
+if __name__ == "__main__":
+    main()
