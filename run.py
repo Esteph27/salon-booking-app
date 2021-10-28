afdfb0a100b4d340dae63434666a318e9dc4bf21
Esteph27 <esteph.csa@gmail.com>
2021-10-28 14:02:47 +0000

    Update client_app values throughout code. Values left to update is total cost

--- run.py
+++ run.py
@@ -42,11 +42,12 @@ client_info = {
 
 # clients_app shows the appointment details for the client
 
-clients_app = {
+client_app = {
   "client name": [],
   "services requested": [],
   "appointment date": [],
-  "booking number": []
+  "booking number": [],
+  "total cost" : []
 }
 
 # -------------------------------------------------------------------->
@@ -63,79 +64,92 @@ SPACE = " "
 # -------------------------------------------------------------------->
 # collect customer information;
 
-# print("To book a new appointment, please provide the below information;\n")
-# print("Please provide client's first name.\n")
-# fname = input("First Name:\n")
-# print(SPACE)
+print("To book a new appointment, please provide the below information;\n")
+print("Please provide client's first name.\n")
+fname = input("First Name:\n")
+print(SPACE)
 
-# print("Please provide client's last name.\n")
+print("Please provide client's last name.\n")
 lname = input("Last Name:\n")
-# print(SPACE)
+print(SPACE)
 
-# print("Please provide client's email address.\n")
-# user_email = input("Email:\n")
-# print(SPACE)
+print("Please provide client's email address.\n")
+user_email = input("Email:\n")
+print(SPACE)
 
-# print("Please provide client's contact number.\n")
-# contact_no = input("Contact Number:\n")
-# print(SPACE)
+print("Please provide client's contact number.\n")
+contact_no = input("Contact Number:\n")
+print(SPACE)
 
 # adds client information to client_info
-# client_info.update({"first name": f"{fname}", "last name": f"{lname}", "email": f"{user_email}", "contact number": f"{contact_no}"})
+client_info.update({"first name": f"{fname}", "last name": f"{lname}", "email": f"{user_email}", "contact number": f"{contact_no}"})
 
-# print(DASH.center(50))
+# update client_ app with client name
+client_app.update({"client name":f"{fname} " + f"{lname}"})
 
-# for info in client_info.values():
-#     print(info)
+print(DASH.center(50))
 
-# print(SPACE)
-# print("Client added!")
-# print(DASH.center(50))
-# print("Continue to appointment details...")
-# print(SPACE)
+for info in client_info.values():
+    print(info)
+
+print(SPACE)
+print("Client added!")
+print(DASH.center(50))
+print("Continue to appointment details...")
+print(SPACE)
 
 # -------------------------------------------------------------------->
 # collect appointment information from the user;
 
-# def salon_availability(day):
-#     """gets available day and provides times available for the day"""
 
-#     print("Available time on " + day.upper() + " is: ")
-#     print(salon_dates[day.lower()])
-#     print(user_time)
+def salon_availability(day):
+    """gets available day and provides times available for the day"""
 
-# print("Thank you. Please now enter the services requested, if there are more than one, please separate them by a comma with no spaces.\n")
-# print(SPACE)
-# print("Here is the list of services:\n")
-
-# def list_of_services():
-#   """prints out lsit of services provided by the salon"""
-
-#   salon_list = [str(key) for key in service_type]
-#   print(salon_list)
+    print("Available time on " + day.upper() + " is: ")
+    print(salon_dates[day.lower()])
+    print(user_time)
 
 
-# def calc_cost(service_names):
-#   """works out how much the appointment will cost"""
+print("Thank you. Please now enter the services requested, if there are more than one, please separate them by a comma with no spaces.\n")
+print(SPACE)
+print("Here is the list of services:\n")
+
+
+def list_of_services():
+  """prints out lsit of services provided by the salon"""
+
+  salon_list = [str(key) for key in service_type]
+  print(salon_list)
+
+
+def calc_cost(service_names):
+  """works out how much the appointment will cost"""
  
-#   list_services = service_names.split(",")
+  list_services = service_names.split(",")
 
-#   print(len(list_services))
+  print(len(list_services))
 
-#   total = 0
-#   for i in range(len(list_services)):
-#     print ("the total bill for " + list_services[i] + " is: " + str(service_type[list_services[i]]))
+  total = 0
+  for i in range(len(list_services)):
+    print ("the total bill for " + list_services[i] + " is: " + str(service_type[list_services[i]]))
 
-#     total += service_type[list_services[i]]
+    total += service_type[list_services[i]]
 
-#     print("Total will be: " + str(total))
+    print("Total will be: " + str(total))
 
 
-# user_day = input("Please provide requested day (Monday to Friday):\n")
+# update client_ app with total cost;
+# client_app.update({"total cost": " £" f"{str(total)}"})
 
-# user_time = input("Please select a time for " f"{user_day}" ":\n")
 
-# user_selection = input("Enter services requested, : \n")
+user_day = input("Please provide requested day (Monday to Friday):\n")
+
+user_time = input("Please select a time for " f"{user_day}" ":\n")
+
+user_selection = input("Enter services requested, : \n")
+
+# update client_ app with services input
+client_app.update({"services requested":f" {user_selection}"})
 
 
 # -------------------------------------------------------------------->
@@ -153,13 +167,29 @@ print(random_letter)
 id_num = f"{random_num}" + f"{random_letter}"
 print(id_num)
 
+# update clients_ app with booking number
+client_app.update({"booking number":f" {id_num}"})
+
 # -------------------------------------------------------------------->
 # confirm appointment booking;
+
+print("Booking completed!\nBooking details below:")
+print(client_app)
+
+# clients_app.update({})
+
+# # clients_app = {
+# #   "client name": [],
+# #   "services requested": [],
+# #   "appointment date": [],
+# #   "booking number": [],
+# #   "booking cost" : []
+# # }
 
 # -------------------------------------------------------------------->
 # funcitons:
 
-# salon_availability(user_day)
-# list_of_services()
-# calc_cost(user_selection)
+salon_availability(user_day)
+list_of_services()
+calc_cost(user_selection)
 
