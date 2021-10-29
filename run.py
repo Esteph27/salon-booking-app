e9971ba76a71146c6b503cc4ba767fdc46a5681f
Esteph27 <esteph.csa@gmail.com>
2021-10-29 00:03:23 +0000

    Add for loop to print out client_app items. Add SPACE and DASH throughout code for visual effect

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import random
 import string
 
-"""Salon Booking App"""
+# -------------------------------------------------------------------->
 
+DASH = "-" * 35
+SPACE = " "
+
+# -------------------------------------------------------------------->
 # salon_date show the availability of the salon
 # this will be used to check the salon availability against the user input
 
@@ -11,18 +15,57 @@ salon_dates = {
         "10:00",
         "11:00",
         "12:00",
-        "13:00", 
-        "14:00", 
+        "13:00",
+        "14:00",
         "15:00"
     ],
+    "tuesday": [
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00"
+        ],
 
-  "tuesday": ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00"],
+    "wednesday": [
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00"
+        ],
 
-  "wednesday": ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00"],
+    "thursday": [
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00"
+        ],
 
-  "thursday": ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20;00", "21:00"],
-
-  "friday": ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00"]
+    "friday": [
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+        "21:00"
+        ]
 }
 
 # service_type shows the services and price provided by the salon
@@ -47,7 +90,7 @@ client_info = {
     "contact number": []
 }
 
-# clients_app shows the appointment details for the client
+# clients_app shows the final booking information for the client appointment
 
 client_app = {
   "client name": [],
@@ -57,28 +100,27 @@ client_app = {
   "total cost": []
 }
 
-DASH = "-" * 35
-SPACE = " "
+# -------------------------------------------------------------------->
 
 
 def customer_info():
-    # -------------------------------------------------------------------->
-    # collect customer information;
+    """collect customer information"""
 
+    print(SPACE)
     print("To book a new appointment, please provide the below information;\n")
-    print("Please provide client's first name.\n")
+    print("Please provide client's first name.")
     fname = input_and_check("First Name:\n")
     print(SPACE)
 
-    print("Please provide client's last name.\n")
+    print("Please provide client's last name.")
     lname = input_and_check("Last Name:\n")
     print(SPACE)
 
-    print("Please provide client's email address.\n")
+    print("Please provide client's email address.")
     user_email = input_and_check("Email:\n", field_type="email")
     print(SPACE)
 
-    print("Please provide client's contact number.\n")
+    print("Please provide client's contact number.")
     contact_no = input_and_check("Contact Number:\n", field_type="number")
     print(SPACE)
 
@@ -94,13 +136,16 @@ def customer_info():
     client_app.update({"client name": f"{fname} " + f"{lname}"})
 
     print(DASH.center(50))
+    print(SPACE)
 
     for info in client_info.values():
         print(info)
 
     print(SPACE)
     print("Client added!")
+    print(SPACE)
     print(DASH.center(50))
+    print(SPACE)
     print("Continue to appointment details...")
     print(SPACE)
 
@@ -115,6 +160,7 @@ def appointment_info():
         print(salon_list)
 
     def _calc_cost(selection):
+        """calculate price of appointment based on the service type provided"""
 
         list_services = selection.split(",")
 
@@ -124,69 +170,86 @@ def appointment_info():
         return total
 
     user_day = input("Please provide requested day (Monday to Friday):\n")
+    print(SPACE)
 
     print("Available time on " + user_day.upper() + " is: ")
     print(salon_dates[user_day.lower()])
+    print(SPACE)
 
     user_time = input("Please select a time for " f"{user_day}" ":\n")
-    print(user_time)
-
-    print("Thank you. Please now enter the services requested, if there are more than one, please separate them by a comma with no spaces.\n")
     print(SPACE)
-    print("Here is the list of services:\n")
+
+    print("Thank you. Date and time receieved!\n")
+    print(DASH.center(50))
+    print(SPACE)
+    print("Please now enter the services requested....\n")
+    print(SPACE)
+    print("If more than one service type are required, please separate them by a comma. Do not include spaces between the commas.\n")
+    print("For example: highlights,tint,hair cut")
+    print("Please select from the list provided:\n")
 
     _list_of_services()
+    print(SPACE)
 
-    user_selection = input("Enter services requested: \n")
+    user_selection = input("Enter services requested:\n")
+    print(SPACE)
+    print("Information received!\n")
+    print('Calculating price...')
+    print(SPACE)
+    print(DASH.center(50))
+    print(SPACE)
+    total_cost = _calc_cost(user_selection)
+    print("Total will be: £ " + str(total_cost))
+    print(SPACE)
+    print(DASH.center(50))
+    print(SPACE)
+
+    # update client_ app with user day and time selected
+    client_app.update({"appointment date": f"{user_day}" + f"{user_time}"})
 
     # update client_ app with services input
-    client_app.update({"services requested": f" {user_selection}"})
-
-    total_cost = _calc_cost(user_selection)
-    print("Total will be: " + str(total_cost))
+    client_app.update({"services requested": f"{user_selection}"})
 
     # update client_ app with total cost;
-    # client_app.update({"total cost": " £" f"{str(total)}"})
+    client_app.update({"total cost": f"{str(total_cost)}"})
 
 
 def confirm_booking():
-    # -------------------------------------------------------------------->
-    # generate booking id number;
+    """generate booking id number and confirm booking details"""
 
-    print("Booking ID number:")
-
+    # generates random booking number
     random_num = random.randint(1, 99)
-    print(random_num)
-
     random_letter = random.choice(string.ascii_letters) * 2
-    print(random_letter)
-
-    id_num = f"{random_num}" + f"{random_letter}"
-    print(id_num)
+    id_num = f"{random_num}" + f"{random_letter.upper()}"
 
     # update clients_ app with booking number
-    client_app.update({"booking number": f" {id_num}"})
+    client_app.update({"booking number": f"{id_num}"})
 
-    # -------------------------------------------------------------------->
+    final_booking_info = []
+    for item in client_app.items():
+        final_booking_info.append(item)
+
     # confirm appointment booking;
-
-    print("Booking completed!\nBooking details below:")
-    print(client_app)
-
-    # clients_app.update({})
+    print("Booking completed!\n")
+    print("Final booking details below:\n")
+    print(SPACE)
+    print(final_booking_info)
+    print(SPACE)
+    print(DASH.center(50))
 
 
 def input_and_check(label, field_type=None):
+    """validate user for name, email and phone number inputs"""
 
     while True:
         value = input(label)
 
         if len(value) == 0:
-            print("No entry given, please try again")
+            print("No name provided, please try again")
         elif field_type == "email" and '@' not in value:
-            print("Invalid email, email has to contain @ sign")
+            print("Invalid email, email has to contain an @ symbol")
         elif field_type == "number" and len(value) != 11:
-            print("Invalid number, number has to contain 11 characters")
+            print("Invalid number, number has to contain 11 numbers")
         else:
             return value
 
@@ -194,9 +257,9 @@ def input_and_check(label, field_type=None):
 def main():
     """calls main functions"""
 
+    print(SPACE)
     intro = "Welcome to the Salon Booking App!\n"
     print(intro.center(50))
-
     print(DASH.center(50))
 
     customer_info()
